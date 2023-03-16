2b54c0e3d8e7c8a46ca553caaf2f07dd2f9b7361
krasenHristov <DoAnuSho@gmail.com>
2023-03-16 17:28:18 +0000

    testing bootstrap

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-# adding this to test github
+d1 = {"key" : "value"}
+d2 = {"key" : {"key1" : "value"}}
+d3 = {"key":[{"key2":["this deep",["hello"]]}]}
+
+print(d3["key"][0]["key2"][1][0])
+
+g = [1,2,4,5,6,7,1,2,3,8,9,10]
+
+for ind in range(0,len(g)):
+    if g[ind] == 1 and g[ind+1] == 2 and g[ind+2] == 3:
+        print("Found")
+
