61eb300e423febfa33cd4a672218834e1d0d5bbe
Krasyo <doanusho@gmail.com>
2023-07-13 21:32:40 +0100

    moved the blog project to new repo

--- flask_project/blog/posts/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
-
-class BlogPostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    text = StringField('Text', validators=[DataRequired()])
-    submit = SubmitField('Post')
-
